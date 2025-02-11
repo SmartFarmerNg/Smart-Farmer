d2390a13dd7c9c2f68a907e2cd9aba27aad255cd
Adewole Ayonitemi <adewoleayonitemi@gmail.com>
2025-02-11 17:40:18 +0100

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -21,7 +21,7 @@ const LandingPage = () => {
         <div className='bg-[#E7F6F2] h-screen flex flex-col items-center m-auto gap-6'>
             {isLoading && <Barloader />}
             <div className='container flex flex-col items-center gap-5 bg-white p-5 rounded-lg shadow-2xl shadow-[#00000098] mt-auto sm:w-[70%] lg:w-[50%] xl:w-[30%]'>
-                <h1 className='text-4xl font-bold'>Logo</h1>
+                <h1 className='text-4xl font-bold text-[#0FA280]'>Smart Farmer</h1>
                 <span className='text-md text-center'>Your partner in Smart farming. Get ready to transform your farming experience with Agrovest our app is designed to help you</span>
             </div>
             <button onClick={handleSubmit} className='bg-[#0FA280] hover:bg-[#0fa270] p-3 px-14 font-semibold text-xl rounded-lg mb-auto' >Get Started</button>
