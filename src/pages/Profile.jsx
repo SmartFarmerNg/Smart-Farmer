78cce3fd1b2e424e3763b801d379f2e598982f3b
Adewole Ayonitemi <adewoleayonitemi@gmail.com>
2025-04-17 16:00:57 +0100

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -113,7 +113,13 @@ const Profile = () => {
                                 <motion.button
                                     whileHover={{ scale: 1.05 }}
                                     whileTap={{ scale: 0.9 }}
-                                    className={`mt-4 py-2 px-4 rounded-lg shadow w-full bg-[${accent}] ${accent === '#ECF87F' || accent === '#75E6DA' ? 'text-black' : 'text-white'}`}
+                                    className={`mt-4 py-2 px-4 rounded-lg shadow w-full`}
+                                    style={
+                                        {
+                                            backgroundColor: accent,
+                                            color: accent === '#ECF87F' || accent === '#75E6DA' ? 'black' : 'white',
+                                        }
+                                    }
                                     onClick={() => navigate('/transact/deposit')}
                                 >
                                     Fund Account
