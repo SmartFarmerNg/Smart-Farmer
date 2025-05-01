0e91b30bca98537f251d3dc928d567aedb166c80
Adewole Ayonitemi <adewoleayonitemi@gmail.com>
2025-05-01 22:09:42 +0100

    Update Deposit.jsx

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -162,6 +162,8 @@ const Deposit = () => {
                     <p className="text-center text-sm">Loading user info...</p>
                 ) : (
                     <div className="flex flex-col gap-2">
+                        <p className='text-red-600 font-semibold flex gap-2 items-center mb-2'> <AlertTriangle size={20} /><span>(Deposits is still in development. Funds may be lost.)</span></p>
+
                         <p>
                             Available Balance:{" "}
                             <span className="font-semibold">{formatCurrency(balance)}</span>
